e7b3d71c65647dc0612e8769f212fd4d7322a3ec
Philipp-Marcel Duck <phil.mar.du@gmail.com>
2023-10-10 12:25:07 +0200

    ADD Erstellen Seite angefangen zu designen Drawer um Fragen hinzuzufügen erstellt

---
+++ src/components/AddQuestionDrawer.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Container,
+  Button,
+  useDisclosure,
+  Input,
+} from "@chakra-ui/react";
+
+function AddQuestionDrawer({ onClose, onOpen, isOpen }) {
+  return (
+    <Drawer size={"md"} isOpen={isOpen} placement="right" onClose={onClose}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>Frage hinzufügen</DrawerHeader>
+
+        <DrawerBody></DrawerBody>
+
+        <DrawerFooter>
+          <Button variant="outline" mr={3} onClick={onClose}>
+            Abbrechen
+          </Button>
+          <Button colorScheme="primary">Hinzufügen</Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+export default AddQuestionDrawer;
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import {
+  Container,
+  Button,
+  useDisclosure,
+  Heading,
+  Box,
+  FormControl,
+  FormLabel,
+  Select,
+  Input,
+  Card,
+  CardBody,
+  Flex,
+  Spacer,
+  VStack,
+} from "@chakra-ui/react";
+import AddQuestionDrawer from "../components/AddQuestionDrawer";
+import { MdAdd } from "react-icons/md";
 
 function Create() {
-  return <div>Create</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Card maxW={"800px"}>
+      <CardBody>
+        <VStack gap={"2em"} w={"100%"} align={"start"}>
+          <Heading>Quiz erstellen</Heading>
+
+          <Box w={"100%"}>
+            <form>
+              <FormControl id="title" marginBottom="20px">
+                <FormLabel>Titel:</FormLabel>
+                <Input type="text" placeholder="Geben Sie einen Titel ein" />
+              </FormControl>
+
+              <FormControl id="topic" marginBottom="20px">
+                <FormLabel>Thema auswählen:</FormLabel>
+                <Select placeholder="Wählen Sie ein Thema">
+                  <option value="thema1">Thema 1</option>
+                  <option value="thema2">Thema 2</option>
+                  <option value="thema3">Thema 3</option>
+                </Select>
+              </FormControl>
+            </form>
+          </Box>
+          <Flex w={"100%"} align={"center"}>
+            <Heading size={"md"}>Fragen:</Heading>
+            <Spacer />
+            <Button
+              leftIcon={<MdAdd />}
+              size={"sm"}
+              variant={"outline"}
+              onClick={onOpen}
+            >
+              Frage hinzufügen
+            </Button>
+          </Flex>
+
+          <AddQuestionDrawer
+            onClose={onClose}
+            onOpen={onOpen}
+            isOpen={isOpen}
+          />
+        </VStack>
+      </CardBody>
+    </Card>
+  );
 }
 
 export default Create;

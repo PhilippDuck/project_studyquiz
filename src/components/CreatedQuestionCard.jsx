7925b5981af67cbbe222ca64947463915c9b46b9
Philipp-Marcel Duck <phil.mar.du@gmail.com>
2023-10-11 13:08:31 +0200

    ADD "CreatedQuerstionCard" angepasst

--- src/components/CreatedQuestionCard.jsx
+++ src/components/CreatedQuestionCard.jsx
@@ -1,15 +1,45 @@
 import React from "react";
-import { Card, CardBody, Heading, Text } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Heading,
+  Text,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box,
+} from "@chakra-ui/react";
 
 function CreatedQuestionCard({ question, answers, correctAnswer, hint }) {
   return (
     <Card variant={"outline"} w={"100%"}>
       <CardBody>
-        <Heading size={"md"}>{question}</Heading>
-        {answers.map((answer) => {
-          return <Text>{answer}</Text>;
-        })}
-        <Text>Hinweis: {hint}</Text>
+        <Accordion allowToggle>
+          <AccordionItem border={"none"}>
+            <AccordionButton>
+              <Box as="span" flex="1" textAlign="left">
+                <Heading size={"md"}>{question}</Heading>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+
+            <AccordionPanel pb={4}>
+              Fragen:
+              {answers.map((answer, index) => {
+                return (
+                  <Text
+                    color={index === correctAnswer ? "green.500" : "inherit"}
+                  >
+                    {answer}
+                  </Text>
+                );
+              })}
+              <Text>Hinweis: {hint}</Text>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
       </CardBody>
     </Card>
   );

463e6ed0f58b6b677e1c88c0521307e67237c052
Philipp Duck <phil.mar.du@gmail.com>
2023-10-16 12:16:56 +0200

    ADD Quizze aus DB holen implementiert

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Card, CardBody, Heading, Text, Spinner } from "@chakra-ui/react";
+import { useRealm } from "../provider/RealmProvider";
+import { load } from "webfontloader";
 
 function Games() {
-  return <div>Games</div>;
+  const app = useRealm();
+  const [loadingQuizzes, setLoadingQuizzes] = useState(false);
+  const [quizzes, setQuizzes] = useState([]);
+
+  useEffect(() => {
+    getQuizzes();
+  }, []);
+
+  /**
+   * Holt alle Quiz aus der DB und lädt sie in den State "quizzes"
+   */
+  async function getQuizzes() {
+    setLoadingQuizzes(true);
+    const result = await app.currentUser.functions.getQuizzes();
+    console.log(result);
+    setQuizzes(result);
+    setLoadingQuizzes(false);
+  }
+
+  return (
+    <Card variant={"outline"} maxW={"800px"}>
+      <CardBody>
+        <Heading>Spiele</Heading>
+        {loadingQuizzes ? (
+          <Spinner />
+        ) : (
+          quizzes.map((quiz) => {
+            return <Text key={quiz._id}>{quiz.title}</Text>;
+          })
+        )}
+      </CardBody>
+    </Card>
+  );
 }
 
 export default Games;

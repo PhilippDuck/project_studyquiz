46ec4fa6f455bb10834fee77e05995f4a543044e
Philipp-Marcel Duck <phil.mar.du@gmail.com>
2023-10-30 14:34:36 +0100

    Fix Loading Spinner für letzte Spiele

--- src/components/PlayedQuizzes.jsx
+++ src/components/PlayedQuizzes.jsx
@@ -7,6 +7,7 @@ import {
   Text,
   Card,
   useColorModeValue,
+  Spinner,
 } from "@chakra-ui/react";
 import { useRealm } from "../provider/RealmProvider";
 import formatUnixTimestamp from "../formatUnixTimestamp";
@@ -14,12 +15,14 @@ import formatUnixTimestamp from "../formatUnixTimestamp";
 function PlayedQuizzes() {
   const app = useRealm();
   const [playedQuizzes, setPlayedQuizzes] = useState([]);
+  const [loadingPlayedGames, setLoadingPlayedGames] = useState(false);
 
   useEffect(() => {
     getLastPlayedQuizzes();
   }, []);
 
   async function getLastPlayedQuizzes() {
+    setLoadingPlayedGames(true);
     try {
       const result = await app.currentUser.functions.getPlayedQuizzes();
       console.log(result);
@@ -27,34 +30,40 @@ function PlayedQuizzes() {
     } catch (error) {
       console.log(error);
     }
+    setLoadingPlayedGames(false);
   }
 
   return (
     <Box mt={6} w={"100%"}>
       <Heading size={"md"}>Letzte Spiele:</Heading>
-      <VStack gap={3} mt={6} align={"start"} w={"100%"}>
-        {playedQuizzes.map((playedQuiz) => {
-          return (
-            <Card p={3} key={playedQuiz._id} variant={"outline"} w={"100%"}>
-              <Text fontSize={"xs"}>
-                {formatUnixTimestamp(playedQuiz.endTime)} Uhr
-              </Text>
-              <Text
-                color={useColorModeValue("gray.500", "gray.200")}
-                fontSize={"xs"}
-              >
-                <b>{playedQuiz.playerNick}</b> hat das Quiz{" "}
-                <b>{playedQuiz.quizTitle}</b> mit{" "}
-                <b>{playedQuiz.points} Punkten</b> in{" "}
-                <b>
-                  {(playedQuiz.endTime - playedQuiz.startTime) / 1000} Sekunden
-                </b>{" "}
-                beendet.
-              </Text>
-            </Card>
-          );
-        })}
-      </VStack>
+      {loadingPlayedGames ? (
+        <Spinner />
+      ) : (
+        <VStack gap={3} mt={6} align={"start"} w={"100%"}>
+          {playedQuizzes.map((playedQuiz) => {
+            return (
+              <Card p={3} key={playedQuiz._id} variant={"outline"} w={"100%"}>
+                <Text fontSize={"xs"}>
+                  {formatUnixTimestamp(playedQuiz.endTime)} Uhr
+                </Text>
+                <Text
+                  color={useColorModeValue("gray.500", "gray.200")}
+                  fontSize={"xs"}
+                >
+                  <b>{playedQuiz.playerNick}</b> hat das Quiz{" "}
+                  <b>{playedQuiz.quizTitle}</b> mit{" "}
+                  <b>{playedQuiz.points} Punkten</b> in{" "}
+                  <b>
+                    {(playedQuiz.endTime - playedQuiz.startTime) / 1000}{" "}
+                    Sekunden
+                  </b>{" "}
+                  beendet.
+                </Text>
+              </Card>
+            );
+          })}
+        </VStack>
+      )}
     </Box>
   );
 }
